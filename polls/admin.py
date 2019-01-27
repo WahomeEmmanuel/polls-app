0694087ddee18c263fc564c66c3b951fbc711c47
wahomeemmanuel <gathage.ew@gmail.com>
2019-01-27 21:17:54 +0300

    Add serializers

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Poll, Choice
+
+admin.site.register(Poll)
+admin.site.register(Choice)
 
 # Register your models here.
--- polls/urls.py
+++ polls/urls.py
@@ -1,3 +1,7 @@
-urlpatterns = [
+from django.urls import path
+from .apiviews import PollList, PollDetail
 
+urlpatterns = [
+    path("polls/", PollList.as_view(), name="polls_list"),
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail")
 ]
